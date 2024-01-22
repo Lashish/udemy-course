0e00896bf115fe8581938f64d3fe93ff54322849
Lashish <ashishklite@gmail.com>
2024-01-22 16:03:23 +0530

    user data

---
+++ flashcard/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import InputBox from "./compnents/InputBox";
+
+function App() {
+  return (
+    <div className="App-header">
+      <div className="container">
+        <InputBox />
+      </div>
+    </div>
+  );
+}
+export default App;

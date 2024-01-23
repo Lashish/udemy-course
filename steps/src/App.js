6c8b31d76399a58a31a781fadbfa18e4e063162e
Lashish <ashishklite@gmail.com>
2024-01-24 05:12:11 +0530

    Complete steps project

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,16 +1,31 @@
+import{useState} from 'react'
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+  const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
+  const handlePrevious=()=>setStep(step>1&&step<=3?step-1:step)
+  const handleNext = ()=>  setStep(step<3?step+1:step)
   return (
+    <div>
+      <button onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+    {isOpen && (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active":""}`}>1</div>
+        <div className={`${step >= 2 ? "active":""}`}>2</div>
+        <div className={`${step >= 3 ? "active":""}`}>3</div>
       </div>
-      <div className="message">Hello</div>
+      <div className="message">step{step}: {messages[step-1]}</div>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handlePrevious}>Previous</button>
+        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handleNext}>Next</button>
       </div>
+    </div>)}
     </div>
   );
 }
---
+++ ui/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+        <h1>Fast React Pizza Co.</h1>
+        {/* <Search/> */}
+        <p>GoTo</p>
+    </div>
+  )
+}
+
+export default Header

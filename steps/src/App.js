1953ac802929304e75f3a05831ddd383b8d3127f
Lashish <ashishklite@gmail.com>
2024-02-08 03:14:52 +0530

    07-02-2024

--- steps/src/App.js
+++ steps/src/App.js
@@ -22,11 +22,15 @@ function App() {
       </div>
       <div className="message">step{step}: {messages[step-1]}</div>
       <div className="buttons">
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handlePrevious}>Previous</button>
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handleNext}>Next</button>
+        <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious} ><span>👈</span>Previous</Button>
+        <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext} >Next<span>👉</span></Button>
       </div>
     </div>)}
     </div>
   );
+}
+
+function Button({textColor, bgColor, onClick , children}){
+  return<button style={{backgroundColor:bgColor, color:textColor}} onClick={onClick}>{children}</button>
 }
 export default App;
